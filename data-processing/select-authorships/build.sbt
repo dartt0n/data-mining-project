b1d0695f84f6203499628b90fedfe05628b9f6c2
Anton Kudryavtsev <dartt0n@ya.ru>
2025-05-09 14:10:29 +0300

    feat: select authors

---
+++ data-processing/select-authorships/build.sbt
@@ -0,0 +1,14 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.12.20"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "select-authorships",
+    libraryDependencies ++= Seq(
+      "org.apache.spark" %% "spark-core"      % "3.5.5" % "provided",
+      "org.apache.spark" %% "spark-sql"       % "3.5.5" % "provided",
+      "org.apache.spark" %% "spark-streaming" % "3.5.5" % "provided",
+      "org.apache.hadoop" % "hadoop-aws"      % "3.4.1"
+    )
+  )
---
+++ data-processing/select-authorships/src/main/scala/Main.scala
@@ -0,0 +1,44 @@
+import org.apache.spark.sql._
+import org.apache.spark.sql.{functions => F}
+import org.apache.spark.sql.types._
+
+object Main {
+  implicit class ColumnMethods(private val c: Column) {
+    def then(t: Column => Column): Column = t(c)
+  }
+
+  def trimPrefix(prefixStr: String)(c: Column) = F
+    .when(c.startsWith(prefixStr), c.substr(F.lit(prefixStr.length()), F.length(c)))
+    .otherwise(c)
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .appName("preprocessing")
+      .getOrCreate
+
+    val sc = spark.sparkContext
+
+    sc.hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    sc.hadoopConfiguration.set("fs.s3a.endpoint", "s3.yandexcloud.net")
+    sc.hadoopConfiguration.set("fs.s3a.access_key", sys.env("S3A_ACCESS_KEY"))
+    sc.hadoopConfiguration.set("fs.s3a.secret_key", sys.env("S3A_SECRET_KEY"))
+
+    spark.read
+      .parquet("s3a://openalex/openalex")
+      .select(F.col("authors"), F.col("id") as "work")
+      .filter(F.size(F.col("authors")) > 1)
+      .withColumn("author", F.explode(F.col("authors")))
+      .select(F.col("author.id") as "author", F.col("work"))
+      .groupBy("author")
+      .agg(F.collect_set(F.col("work")) as "works")
+      .filter(F.size(F.col("works")) > 1)
+      .withColumn("work", F.explode(F.col("works")))
+      .groupBy("work")
+      .agg(F.collect_set(F.col("author")) as "authors")
+      .write
+      .mode("overwrite")
+      .parquet("s3a://dataprocdata/authors-works")
+  }
+}
